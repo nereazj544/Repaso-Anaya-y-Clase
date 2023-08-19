9a4f37a1ede766e94b08b049ec36d9da6c11f789
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2023-08-19 19:53:39 +0200

    (C2-2) Impar o Par (original + con scanner)
    Lo que hace el programa es que te indentifica si es par o impar, si algun numero por ejemplo: 2465 contiene algun impar devolvera que es "verdadero" por el simple echo de tener un 5. 
    En caso contrario devolvera "False" ya que si ponemos un numero que este compuesto por solo pares por ejemplo: 2408. nos dira que es "falso".
    Antes de iniciar el programa se debe de especifiar el numero de cadenas que vamos a introducir. Es decir: 
    > Te a decir que intruducas un numero para realizar dicho nº de cadenas:
    >> Pones 5; nos devolvera:
    >>>Cadean 1 ~ Cadena 5 (en las cuales tenemos que introducir los numeros a eleccion, simpre que detecte algun numero impar devolvera verdadero si o si)

--- Java/clase/ejercicos/colecciones/Colecciones 2/colecciones.java
+++ Java/clase/ejercicos/colecciones/Colecciones 2/colecciones.java
@@ -30,22 +30,43 @@ public class colecciones {
      */
 
     // ESTO PARA USAR EL SCANNER
-    public static void eliminarLasDeLongitudPar(Set<String> conSet) {
-        // recorre el conjunto de cadenas
-        Iterator<String> iterator = conSet.iterator();
+    /*
+     * 
+     * public static void eliminarLasDeLongitudPar(Set<String> conSet) {
+     * // recorre el conjunto de cadenas
+     * Iterator<String> iterator = conSet.iterator();
+     * 
+     * // Crea un conjunto auxiliar para almacenar cadenas a eliminar
+     * Set<String> cadenasAEliminar = new HashSet<>();
+     * 
+     * // Verifica la longitud es par y agrega a conjunto auxiliar
+     * while (iterator.hasNext()) {
+     * String cadena = iterator.next();
+     * if (cadena.length() % 2 == 0) {
+     * cadenasAEliminar.add(cadena);
+     * }
+     * }
+     * // Elimina las cadenas del conjunto original
+     * conSet.removeAll(cadenasAEliminar);
+     * }
+     */
 
-        // Crea un conjunto auxiliar para almacenar cadenas a eliminar
-        Set<String> cadenasAEliminar = new HashSet<>();
+    /*
+     * EJERCICO 2: Método llamado contieneImpares que acepte un conjunto de números
+     * enteros y retorne true si contiene al menos un número impar o false en caso
+     * contrario.
+     */
 
-        // Verifica la longitud es par y agrega a conjunto auxiliar
-        while (iterator.hasNext()) {
-            String cadena = iterator.next();
-            if (cadena.length() % 2 == 0) {
-                cadenasAEliminar.add(cadena);
+    public static boolean contieneImpares(Set<Integer> conjunto) {
+        // Esto es para verificar si es par o impar
+        for (int numero : conjunto) {
+            if (numero % 2 != 0) {
+                // si tiene al menos un numero impar(1,3,5,7,9,11) va a decir que verdadero
+                return true;
             }
         }
-        // Elimina las cadenas del conjunto original
-        conSet.removeAll(cadenasAEliminar);
+        // NO tiene impares, pero si tiene pares(2,4,6,8,10)
+        return false;
     }
 
     public static void main(String[] args) {
@@ -63,24 +84,49 @@ public class colecciones {
          */
 
         // EL MISMO EJERCICO PERO CON SCANNER
-        Set<String> conjuntoOSet = new HashSet<>();
-        Scanner s = new Scanner(System.in);
+        /*
+         * Set<String> conjuntoOSet = new HashSet<>();
+         * Scanner s = new Scanner(System.in);
+         * System.out.println("Escribe la cantidad de cadenas, es decir, un numero: ");
+         * int cantidad = Integer.parseInt(s.nextLine());
+         * for (int i = 0; i < cantidad; i++) {
+         * System.out.println("Cadena " + (i + 1) + ": ");
+         * String cadena = s.nextLine();
+         * conjuntoOSet.add(cadena);
+         * }
+         * eliminarLasDeLongitudPar(conjuntoOSet);
+         * System.out.println("Conjunto después de eliminar cadenas de longitud par: ");
+         * for (String cadena : conjuntoOSet) {
+         * System.out.println(cadena);
+         * }
+         * 
+         */
 
-        System.out.println("Escribe la cantidad de cadenas, es decir, un numero: ");
+        // ----------------------------------------------------------------------------
+
+        // EJERCICIO 2 (COMO MANDA EL ENUNCIADO)
+        /*
+         * Set<Integer> conjunto = new HashSet<>();
+         * conjunto.add(2);
+         * conjunto.add(6);
+         * conjunto.add(4);
+         * conjunto.add(8);
+         * 
+         * System.out.println("Continene: " + contieneImpares(conjunto));
+         * 
+         */
+        //Ejercicio 2 (CON SCANNER)
+        
+        Scanner s = new Scanner(System.in);
+        Set<Integer> conjunto = new HashSet<>();
+        System.out.print("Introduce un numero para iniciar la cadena con dicho numero: ");
         int cantidad = Integer.parseInt(s.nextLine());
 
-        for (int i = 0; i < cantidad; i++) {
-            System.out.println("Cadena " + (i + 1) + ": ");
-            String cadena = s.nextLine();
-            conjuntoOSet.add(cadena);
+        for (int i = 0; i < cantidad; i++){
+            System.out.println("Numero " + (i + 1) + ": ");
+            int numero = Integer.parseInt(s.nextLine());
+            conjunto.add(numero);
         }
-
-        eliminarLasDeLongitudPar(conjuntoOSet);
-
-        System.out.println("Conjunto después de eliminar cadenas de longitud par: ");
-        for (String cadena : conjuntoOSet) {
-            System.out.println(cadena);
-        }
-
+        System.out.println("El conjunto contiene impares: " + contieneImpares(conjunto));
     }
 }
